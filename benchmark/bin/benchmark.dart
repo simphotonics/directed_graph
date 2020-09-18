fe5add643e101d68e2d43590d8a4273b3ca11d50
simphotonics <git@simphotonics.com>
2020-09-18 22:30:55 +0100

    Removed usage of ansicolor.

--- benchmark/bin/benchmark.dart
+++ benchmark/bin/benchmark.dart
@@ -1,10 +1,7 @@
-import 'package:ansicolor/ansicolor.dart';
+//import 'package:ansicolor/ansicolor.dart';
 import 'package:benchmark_harness/benchmark_harness.dart';
 import 'package:directed_graph/directed_graph.dart';
-
-AnsiPen magenta = AnsiPen()..magenta(bold: true);
-AnsiPen green = AnsiPen()..green(bold: true);
-AnsiPen blue = AnsiPen()..blue(bold: true);
+import 'package:directed_graph/src/utils/color_utils.dart';
 
 int comparator(
   String s1,
@@ -25,22 +22,6 @@ class DirectedGraphBenchmark extends BenchmarkBase {
           'f': {'i'},
           'i': {'l'},
           'k': {'g', 'f'},
-          // 'a1': ['b', 'h', 'c', 'e'],
-          // 'b1': ['h'],
-          // 'c1': ['h', 'g'],
-          // 'd1': ['e', 'f'],
-          // 'e1': ['g'],
-          // 'f1': ['i'],
-          // 'i1': ['l'],
-          // 'k1': ['g', 'f'],
-          // 'a12': ['b', 'h', 'c', 'e'],
-          // 'b12': ['h'],
-          // 'c12': ['h', 'g'],
-          // 'd12': ['e', 'f'],
-          // 'e12': ['g'],
-          // 'f12': ['i'],
-          // 'i12': ['l'],
-          // 'k12': ['g', 'f'],
         }, comparator: comparator),
         super(name);
 
@@ -166,32 +147,36 @@ class GraphManipulation extends DirectedGraphBenchmark {
 //   /// Not measured teardown code executed after the benchmark runs.
 //   @override
 //   void teardown() {
-//     print(magenta('\nShortest Path (graphs) ... '));
+//     print(magenta('\nShortest Path (graphs)import 'package:directed_graph/graph_crawler.dart'; ... '));
 //     print(green(graph.shortestPath('d', 'l').toString()));
 //   }
 // }
 
-class PathsTest extends DirectedGraphBenchmark {
-  PathsTest(String name) : super(name);
+class PathsBenchmark extends DirectedGraphBenchmark {
+  PathsBenchmark(String name) : super(name);
+
+  List<List<String>>? paths;
+  GraphCrawler<String>? crawler;
 
   @override
   void setup() {
     super.setup();
+    graph.addEdges('i', {'k'});
+    crawler = GraphCrawler(graph.edges);
   }
-
-  List<List<String>>? paths;
 
   /// The benchmark code.
   @override
   void run() {
-    paths = graph.paths('d', 'l');
+    paths = crawler!.paths('d', 'l');
   }
 
   /// Not measured teardown code executed after the benchmark runs.
   @override
   void teardown() {
-    print(magenta('\ngraph.paths(d, l) ... '));
+    print(magenta('\ncrawler!.paths(d, l) ... '));
     print(green(paths.toString()));
+    graph.removeEdges('i', {'k'});
   }
 }
 
@@ -221,28 +206,58 @@ class GraphCycle extends DirectedGraphBenchmark {
   }
 }
 
-class GraphFindCycle extends DirectedGraphBenchmark {
-  GraphFindCycle(String name) : super(name);
+class FastPathsBenchmark extends DirectedGraphBenchmark {
+  FastPathsBenchmark(String name) : super(name);
 
-  List<String>? paths;
+  List<List<String>>? paths;
+  GraphCrawler<String>? crawler;
 
   @override
   void setup() {
     super.setup();
     graph.addEdges('i', {'k'});
+    crawler = GraphCrawler(graph.edges);
   }
 
   /// The benchmark code.
   @override
   void run() {
-    paths = graph.findCycle();
+    paths = crawler!.fastPaths('d', 'l');
   }
 
   /// Not measured teardown code executed after the benchmark runs.
   @override
   void teardown() {
-    print(magenta('\ngraph.findCycle() ... '));
+    print(magenta('\ncrawler!.fastPaths(d,l) ... '));
     print(green(paths.toString()));
+    graph.removeEdges('i', {'k'});
+  }
+}
+
+class WalksBenchmark extends DirectedGraphBenchmark {
+  WalksBenchmark(String name) : super(name);
+
+  List<List<String>>? walks;
+  GraphCrawler<String>? crawler;
+
+  @override
+  void setup() {
+    super.setup();
+    graph.addEdges('i', {'k'});
+    crawler = GraphCrawler(graph.edges);
+  }
+
+  /// The benchmark code.
+  @override
+  void run() {
+    walks = crawler!.walks('d', 'l', maxCount: 2);
+  }
+
+  /// Not measured teardown code executed after the benchmark runs.
+  @override
+  void teardown() {
+    print(magenta('\ncrawler!.walks(d, l, maxCount: 2) ... '));
+    print(green(walks.toString()));
     graph.removeEdges('i', {'k'});
   }
 }
@@ -256,9 +271,10 @@ void main() {
   var localSourcesBenchmark = LocalSources('Local Sources:');
   var graphManipulation = GraphManipulation('Removing/Adding Vertices');
   // var shortestPath = ShortestPath('ShortestPath');
-  var crawlerTest = PathsTest('PathsTest');
+  var pathsBenchmark = PathsBenchmark('PathsTest');
   var graphCycle = GraphCycle('GraphCycle');
-  var graphFindCycle = GraphFindCycle('GraphFindCycle');
+  var fastPathsBenchmark = FastPathsBenchmark('FastPaths');
+  var walksBenchmark = WalksBenchmark('WalksBenchmark');
 
   inDegreeBenchmark.report();
   topDFSBenchmark.report();
@@ -267,7 +283,8 @@ void main() {
   localSourcesBenchmark.report();
   graphManipulation.report();
   // shortestPath.report();
-  crawlerTest.report();
+  pathsBenchmark.report();
+  fastPathsBenchmark.report();
+  walksBenchmark.report();
   graphCycle.report();
-  graphFindCycle.report();
 }
