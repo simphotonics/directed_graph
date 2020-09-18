90798af4ac0a22c49a6d04897ffa5335094f7685
simphotonics <git@simphotonics.com>
2020-09-18 22:27:47 +0100

    Removed usage of ansipen.

--- example/bin/example.dart
+++ example/bin/example.dart
@@ -1,5 +1,5 @@
-import 'package:directed_graph/directed_graph.dart';
-import 'package:ansicolor/ansicolor.dart';
+import 'package:directed_graph/src/base/directed_graph_base.dart';
+import 'package:directed_graph/src/base/graph_crawler_base.dart';
 
 // To run this program navigate to
 // the folder 'directed_graph/example'
@@ -27,72 +27,148 @@ void main() {
     comparator: comparator,
   );
 
-  final bluePen = AnsiPen()..blue(bold: true);
-  final magentaPen = AnsiPen()..magenta(bold: true);
+  // final bluePen = AnsiPen()..blue(bold: true);
+  // final magentaPen = AnsiPen()..magenta(bold: true);
 
-  print(magentaPen('Example Directed Graph...'));
-  print(bluePen('graph.toString():'));
+  // print(magentaPen('Example Directed Graph...'));
+  // print(bluePen('graph.toString():'));
+  // print(graph);
+
+  // print(bluePen('\nIs Acylic:'));
+  // print(graph.isAcyclic);
+
+  // print(bluePen('\nStrongly connected components:'));
+  // //print(graph.stronglyConnectedComponents);
+
+  // print(bluePen('\nShortestPath(d, l):'));
+  // //print(graph.shortestPath('d', 'l'));
+
+  // print(bluePen('\nInDegree(C):'));
+  // print(graph.inDegree('c'));
+
+  // print(bluePen('\nOutDegree(C)'));
+  // print(graph.outDegree('c'));
+
+  // print(bluePen('\nVertices sorted in lexicographical order:'));
+  // print(graph.vertices);
+
+  // print(bluePen('\nVertices sorted in inverse lexicographical order:'));
+  // graph.comparator = inverseComparator;
+  // print(graph.vertices);
+  // graph.comparator = comparator;
+
+  // print(bluePen('\nInDegreeMap:'));
+  // print(graph.inDegreeMap);
+
+  // print(bluePen('\nSorted Topological Ordering:'));
+  // print(graph.sortedTopologicalOrdering);
+
+  // print(bluePen('\nTopological Ordering:'));
+  // print(graph.topologicalOrdering);
+
+  // print(bluePen('\nLocal Sources:'));
+  // print(graph.localSources);
+
+  // // Add edge to render the graph cyclic
+  // graph.addEdges('i', {'k'});
+  // graph.addEdges('l', {'l'});
+  // graph.addEdges('i', {'d'});
+
+  // print(bluePen('\nCycle:'));
+  // print(graph.cycle);
+
+  // final crawler = GraphCrawler(graph.edges);
+
+  // print(bluePen('\nPaths from D to L.'));
+  // print(crawler.paths('d', 'l'));
+
+  // print(bluePen('\nPaths from D to I.'));
+  // print(crawler.paths('d', 'i'));
+
+  // print(bluePen('\nPaths from A to H.'));
+  // print(crawler.paths('a', 'h'));
+
+  // print(bluePen('\nPaths from L to L.'));
+  // print(crawler.paths('l', 'l'));
+
+  // print(bluePen('\nPath from F to F.'));
+  // print(crawler.path('f', 'f'));
+
+  // print(bluePen('\nPaths from A to H.'));
+
+  print('Example Directed Graph...');
+  print('graph.toString():');
   print(graph);
 
-  print(bluePen('\nIs Acylic:'));
+  print('\nIs Acylic:');
   print(graph.isAcyclic);
 
-  print(bluePen('\nStrongly connected components:'));
+  //print('\nStrongly connected components:');
   //print(graph.stronglyConnectedComponents);
 
-  print(bluePen('\nShortestPath(d, l):'));
-  //print(graph.shortestPath('d', 'l'));
+  //print('\nShortestPath(d, l):');
+  //print(graph.shortestPath('d', 'l');
 
-  print(bluePen('\nInDegree(C):'));
+  print('\nInDegree(C):');
   print(graph.inDegree('c'));
 
-  print(bluePen('\nOutDegree(C)'));
+  print('\nOutDegree(C)');
   print(graph.outDegree('c'));
 
-  print(bluePen('\nVertices sorted in lexicographical order:'));
+  print('\nVertices sorted in lexicographical order:');
   print(graph.vertices);
 
-  print(bluePen('\nVertices sorted in inverse lexicographical order:'));
+  print('\nVertices sorted in inverse lexicographical order:');
   graph.comparator = inverseComparator;
   print(graph.vertices);
   graph.comparator = comparator;
 
-  print(bluePen('\nInDegreeMap:'));
+  print('\nInDegreeMap:');
   print(graph.inDegreeMap);
 
-  print(bluePen('\nSorted Topological Ordering:'));
+  print('\nSorted Topological Ordering:');
   print(graph.sortedTopologicalOrdering);
 
-  print(bluePen('\nTopological Ordering:'));
+  print('\nTopological Ordering:');
   print(graph.topologicalOrdering);
 
-  print(bluePen('\nLocal Sources:'));
+  print('\nLocal Sources:');
   print(graph.localSources);
 
   // Add edge to render the graph cyclic
-graph.addEdges('i', {'k'});
-graph.addEdges('l', {'l'});
-graph.addEdges('i', {'d'});
+  graph.addEdges('i', {'k'});
+  graph.addEdges('l', {'l'});
+  //graph.addEdges('i', {'d'});
 
-  print(bluePen('\nCycle:'));
+  print('\nCycle:');
   print(graph.cycle);
 
-  print(bluePen('\nPaths from D to L.'));
-  print(graph.paths('d', 'l'));
+  final crawler = GraphCrawler(graph.edges);
 
-  print(bluePen('\nPaths from D to I.'));
-  print(graph.paths('d', 'i'));
+  print('\nPaths from D to L.');
+  print(crawler.paths('d', 'l'));
 
-  print(bluePen('\nPaths from A to H.'));
-  print(graph.paths('a', 'h'));
+  print('\nPaths from D to I.');
+  print(crawler.paths('d', 'i'));
 
-  print(bluePen('\nPaths from L to L.'));
-  print(graph.paths('l', 'l'));
+  print('\nPaths from A to H.');
+  print(crawler.paths('a', 'h'));
 
-  print(bluePen('\nPath from F to F.'));
-  print(graph.path('f', 'f'));
+  print('\nPaths from L to L.');
+  print(crawler.paths('l', 'l'));
 
-  print(bluePen('\nPaths from A to H.'));
-  //print(graph.shortestPath('a', 'h'));
-  //print(graph.shortestPath(d, k));
+  print('\nPath from F to F.');
+  print(crawler.path('f', 'f'));
+
+  print('\nSimple Tree: Root D');
+  print(crawler.simpleTree('d', target: 'i'));
+
+  print('\nTree: Root D');
+  print(crawler.tree('d', target: 'k', maxCount: 2));
+
+  print('\nWalks: Root D');
+  print(crawler.walks('d','g',maxCount: 2));
+
+
+
 }
