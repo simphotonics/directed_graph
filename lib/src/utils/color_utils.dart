ea7f0b2639d49469f3066c2c70253a5024da41e8
simphotonics <git@simphotonics.com>
2020-12-30 10:43:23 +0000

    Added colour magenta.

--- lib/src/utils/color_utils.dart
+++ lib/src/utils/color_utils.dart
@@ -4,6 +4,8 @@ const String RESET = '\u001B[0m';
 /// Ansi color modifier.
 const String BLUE = '\u001B[1;94m';
 
+const String MAGENTA = '\u001B[1;95m';
+
 /// Ansi color modifier.
 const String CYAN = '\u001B[36m';
 
@@ -16,5 +18,6 @@ const String RED = '\u001B[31m';
 /// Ansi color modifier.
 const String YELLOW = '\u001B[33m';
 
-String magenta(String input) => '$BLUE$input$RESET';
+String magenta(String input) => '$MAGENTA$input$RESET';
 String green(String input) => '$GREEN$input$RESET';
+String blue(String input) => '$BLUE$input$RESET';
