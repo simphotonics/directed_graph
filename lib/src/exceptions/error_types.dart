e14005150585795d5860ecddb9211a6afbcf42c9
simphotonics <git@simphotonics.com>
2021-03-18 10:10:31 +0000

    Added error type.

--- lib/src/exceptions/error_types.dart
+++ lib/src/exceptions/error_types.dart
@@ -8,3 +8,10 @@ class InvalidPath extends ErrorType {}
 
 /// The given pair of vertices do not form a graph edge.
 class NotAnEdge extends ErrorType {}
+
+/// Error thrown while attempting to sort objects of type `T` if:
+/// * `T` does not extend `Comparable` **or**
+/// * a valid `Comparator<T>` was not specified.
+class SortingNotSupported<T> extends ErrorType {
+  Type type = T;
+}
